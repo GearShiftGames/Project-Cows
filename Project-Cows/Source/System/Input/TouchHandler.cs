9105f6a20c4cc856f5decadb223fd7bcc366c7cf
Dean Sinclair <deano.sinclair@yahoo.co.uk>
2016-01-07 07:18:18 +0000

    Implemented TouchHandler and TouchZone classes.

---
+++ Project-Cows/Source/System/Input/TouchHandler.cs
@@ -0,0 +1,42 @@
+﻿// Project Cows -- GearShift Games
+// Written by D. Sinclair, 2015
+// ================
+// TouchHandler.cs
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+
+namespace Project_Cows.Source.System.Input {
+	class TouchHandler {
+		// Deals with any touch input and makes the data accessible
+		// ================
+
+		// Variables
+		private TouchCollection m_collection;
+		private List<TouchLocation> m_touches;
+
+
+		// Methods
+		public TouchHandler() {
+			// TouchHandler constructor
+			m_touches = new List<TouchLocation>();
+			Update();
+		}
+
+		public void Update() {
+			m_collection = TouchPanel.GetState();
+			m_touches.Clear();
+
+			foreach(TouchLocation tl in m_collection) {
+				m_touches.Add(tl);
+			}
+		}
+
+		// Getters
+		// TODO: Possibly re-write method to return a single touch, and then remove it from the array
+		public List<TouchLocation> GetTouches() { return m_touches; }
+	}
+}
---
+++ Project-Cows/Source/System/Input/TouchZone.cs
@@ -0,0 +1,59 @@
+﻿// Project Cows -- GearShift Games
+// Written by D. Sinclair, 2015
+// ================
+// TouchZone.cs
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace Project_Cows.Source.System.Input {
+	class TouchZone {
+		// Sets a rectangular zone which can check if a coordinate in within it
+		// ================
+
+		// Variables
+		private Vector2 m_min;
+		private Vector2 m_max;
+
+
+		// Methods
+		public TouchZone(Vector2 min_, Vector2 max_) {
+			// TouchZone constructor
+			SetMin(min_);
+			SetMax(max_);
+		}
+
+		public TouchZone(TouchZone zone_) {
+			// TouchZone constructor
+			SetMin(zone_.GetMin());
+			SetMax(zone_.GetMax());
+		}
+
+		public bool IsInsideZone(Vector2 position_) {
+			// Checks whether the position is within the touch zone
+			if(position_.X < m_min.X)
+				return false;
+			if(position_.X > m_max.X)
+				return false;
+			if(position_.Y < m_min.Y)
+				return false;
+			if(position_.Y > m_max.Y)
+				return false;
+
+			return true;
+		}
+
+
+		// Getters
+		public Vector2 GetMin() { return m_min; }
+
+		public Vector2 GetMax() { return m_max; }
+
+
+		// Setters
+		public void SetMin(Vector2 min_) { m_min = min_; }
+
+		public void SetMax(Vector2 max_) { m_max = max_; }
+	}
+}
