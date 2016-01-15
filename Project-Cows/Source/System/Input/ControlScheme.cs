d8edfc5ce79437784e8a5fba569220526eedacf1
Dean Sinclair <deano.sinclair@yahoo.co.uk>
2016-01-15 16:01:18 +0000

    Implemented initial ControlScheme class (no real functionality yet).

---
+++ Project-Cows/Source/System/Input/ControlScheme.cs
@@ -0,0 +1,49 @@
+﻿// Project Cows -- GearShift Games
+// Written by D. Sinclair, 2016
+// ================
+// ControlScheme.cs
+
+using System;
+using System.Collections.Generic;
+
+namespace Project_Cows.Source.System.Input {
+    class ControlScheme {
+        // Interface to take user input and return the control output
+        // ================
+
+        // Variables
+        private double m_steeringValue;                 // Steering value, between -1 (left) and 1 (right)
+        private bool m_braking;                         // True/False whether the car is braking
+        
+
+        // Methods
+        public ControlScheme() {
+            // ControlScheme constructor
+            // ================
+
+            m_steeringValue = 0;
+            m_braking = false;
+        }
+
+        public void Update(ref TouchHandler touchHandler_) {
+            // Updates the variables
+            // ================
+
+        }
+
+        private void CalculateSteeringValue() {
+            // Processes inputs to get the steering value
+            // ================
+
+        }
+
+
+        // Getters
+        public double GetSteeringValue() { return m_steeringValue; }
+
+        public bool GetBraking() { return m_braking; }
+
+        // Setters
+
+    }
+}
